b8ee9e54b5ec5fabb6a429512055addd6a4570c0
cooptothe <edward.cooper97@gmail.com>
2025-01-14 11:38:58 -0600

    about us page added

--- screens/AboutUs.js
+++ screens/AboutUs.js
@@ -1,53 +1,15 @@
-import React from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
-import Pdf from 'react-native-pdf';
+import React from "react";
+import { WebView } from "react-native-webview";
+import BackButton from "../components/BackButton";
 
 const AboutUs = () => {
-  // If you want to load the PDF from a local file in iOS, use require here.
-  const source = require('../assets/AboutUsPDF.pdf');
-
-  const handleLoadComplete = (numberOfPages, filePath) => {
-    console.log(`Number of pages: ${numberOfPages}`);
-  };
-
-  const handlePageChanged = (page, numberOfPages) => {
-    console.log(`Current page: ${page}`);
-  };
-
-  const handleError = (error) => {
-    console.log(error);
-  };
-
-  const handleLinkPress = (uri) => {
-    console.log(`Link pressed: ${uri}`);
-  };
-
   return (
-    <View style={styles.container}>
-      <Pdf
-        source={source}
-        onLoadComplete={handleLoadComplete}
-        onPageChanged={handlePageChanged}
-        onError={handleError}
-        onPressLink={handleLinkPress}
-        style={styles.pdf}
-      />
-    </View>
+    <>
+    <BackButton />
+     <WebView style={{ flex: 1 }} source={{ uri: "https://harlequin-tine-85.tiiny.site/" }} />   
+    </>
+
   );
 };
 
 export default AboutUs;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
